4d77aa7b56abc63e0fd9e08cb0155d6a1cd43e84
fgvlasov <fgvlasov@gmail.com>
2025-08-31 13:24:52 +0300

    remove help

--- display.js
+++ display.js
@@ -99,7 +99,7 @@ function next(problem, useAnimation) {
   var problem_type = problem.type;
   var problem_title = problem_type + " - " + problem.first;
 
-  document.title = "#" + problem.problemid;
+  document.title = "Шахматная задача #" + problem.problemid;
   if ("o" in url_parameters) problem_title = "#" + problem.problemid + " " + problem_title;
 
   var titleEl = document.querySelector("#problem-title");
